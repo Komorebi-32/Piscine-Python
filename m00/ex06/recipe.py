707f9fb8dda1972f4b9a3a5dbfcf3126a8354996
bkaras-g <bkaras-g@student.42.fr>
2026-07-21 14:42:51 +0200

    m00ex06 ok

--- m00/ex06/recipe.py
+++ m00/ex06/recipe.py
@@ -60,8 +60,44 @@ def add_recipe():
         print("Prep time must be an integer")
 
 
+def print_menu():
+    print("List of available options:")
+    print("   1: Add a recipe")
+    print("   2: Delete a recipe")
+    print("   3: Print a recipe")
+    print("   4: Print the cookbook")
+    print("   5: Quit\n")
+
+
 if __name__ == "__main__":
-    cookbook.pop("Cake")
-    add_recipe()
-    s = input("Choose a recipe to show details\n")
-    print_recipe_details(s)
+    print("Welcome to the Python Cookbook !")
+    print_menu()
+    choice = 0
+    while choice != 5:
+        try:
+            choice = int(input("Please select an option:\n"))
+        except ValueError:
+            print("Sorry, this option does not exist.")
+            print_menu()
+            continue
+        match choice:
+            case 1:
+                add_recipe()
+            case 2:
+                print("\nAvailable recipes in the cookbook:")
+                print_recipe_names()
+                s = input("\nEnter a recipe to delete:\n")
+                delete_recipe(s)
+            case 3:
+                print("\nAvailable recipes in the cookbook:")
+                print_recipe_names()
+                s = input("\nEnter a recipe to print:\n")
+                print_recipe_details(s)
+            case 4:
+                for x in cookbook.items():
+                    print(x)
+            case 5:
+                print("Cookbook closed. Goodbye !")
+            case _:
+                print("Sorry, this option does not exist.")
+                print_menu()
