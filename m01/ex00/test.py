7ccbb042cc41bdfaa0e652b30c829eb64c36b97a
bkaras-g <bkaras-g@student.42.fr>
2026-07-22 15:27:40 +0200

    m01ex00: add more recipes to tests

--- m01/ex00/test.py
+++ m01/ex00/test.py
@@ -6,13 +6,24 @@ from datetime import datetime
 lasagna = Recipe("Lasagnas", 3, 64, ["eggs", "An Italian Mom", "a pinch of love"], "The best lasagnas in the world", "lunch")
 bruschetta = Recipe("Bruschettas", 1, 3, ["mozza", "tomato", "gooood bread"], "Simple is Beautiful", "starter")
 tiramisu = Recipe("Tiramisu", 4, 320, ["mascarpone al dente"], "Gimme Gimme Gimme Moore", "dessert")
+
+caprese = Recipe("Caprese", 1, 10, ["mozzarella", "tomato", "basil"], "Classic Italian salad", "starter")
+arancini = Recipe("Arancini", 3, 45, ["rice", "mozzarella", "breadcrumbs"], "Sicilian fried rice balls", "starter")
+
+risotto = Recipe("Risotto alla Milanese", 4, 35, ["rice", "saffron", "parmesan"], "Creamy saffron risotto", "lunch")
+osso_buco = Recipe("Osso Buco", 5, 120, ["veal shanks", "white wine", "vegetables"], "Braised veal specialty from Milan", "lunch")
+
+panna_cotta = Recipe("Panna Cotta", 2, 20, ["cream", "sugar", "gelatin"], "Silky Italian dessert", "dessert")
+cannoli = Recipe("Cannoli", 4, 60, ["ricotta", "flour", "sugar"], "Crispy Sicilian pastry", "dessert")
+
 recipes_list = {
-    "starter": bruschetta,
-    "lunch": lasagna,
-    "dessert": tiramisu
+    "starter": [bruschetta, caprese, arancini]
+    "lunch": [lasagna, risotto, osso_buco]
+    "dessert": [tiramisu, panna_cotta, cannoli]
 }
 yummy_book = Book("Yummy Book!", datetime.now(), recipes_list)
 
 yummy_book.get_recipe_by_name("Lasagnas")
 yummy_book.get_recipe_by_name("Tiramisu")
 yummy_book.get_recipe_by_name("abc")
+
