646cbdb03de5f6bacaa9cb9baa8c28562c23f913
bkaras-g <bkaras-g@student.42.fr>
2026-07-23 11:35:05 +0200

    m03ex00 random() ok

--- m03/ex00/NumPyCreator.py
+++ m03/ex00/NumPyCreator.py
@@ -10,8 +10,16 @@ class NumPyCreator:
 
     def from_iterable(self, itr):
         return np.fromiter(itr, float)
-    
-    def from_shape(self, shape, value):
+
+    def from_shape(self, shape, value=0):
+        """returns an array filled with the same value.
+        The first argument is a tuple which specifies the shape of the array, and the second
+        argument specifies the value of the elements. This value must be 0 by default."""
         return np.full(shape, value)
         # find how to fill the array with same values
         # np.reshape(shape[0], shape[1])
+
+    def random(self, shape):
+        """returns an array filled with random values. It takes as an
+        argument a tuple which specifies the shape of the array."""
+        return np.random.rand(*shape)
--- m03/ex00/test.py
+++ m03/ex00/test.py
@@ -1,4 +1,11 @@
 from NumPyCreator import NumPyCreator
 
+print("from_shape((32, 32), 32)\n")
 array = NumPyCreator().from_shape((32, 32), 32)
+print(array)
+print("\nfrom_shape((3, 3))\n")
+array = NumPyCreator().from_shape((3, 3))
+print(array)
+print("\nempty(4, 4)\n")
+array = NumPyCreator().random((4, 4))
 print(array)
