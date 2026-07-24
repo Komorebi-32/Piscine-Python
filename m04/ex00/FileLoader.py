991fe064a034f7353c065d3c22049bae7220211f
bkaras-g <bkaras-g@student.42.fr>
2026-07-24 17:42:54 +0200

    m04ex00 ok

--- m04/ex00/FileLoader.py
+++ m04/ex00/FileLoader.py
@@ -3,13 +3,34 @@ import pandas as pandas
 
 class FileLoader:
     def load(self, path):
+        """takes as an argument the file path of the dataset to load,
+        displays a message specifying the dimensions of the dataset (e.g. 340 x 500) and
+        returns the dataset loaded as a pandas.DataFrame."""
         if not isinstance(path, str):
             print("Error: path should be a string")
             return None
         try:
-            file = pandas.read_csv(path)
+            df = pandas.read_csv(path)
         except FileNotFoundError:
             print("Error: non existing file")
             return None
-        print(f"Loading dataset of dimensions {file.shape[0]} x {file.shape[1]}")
-        return file
+        print(f"Loading dataset of dimensions {df.shape[0]} x {df.shape[1]}") # rows x columns
+        return df
+
+    def display(self, df, n):
+        """takes a pandas.DataFrame and an integer as arguments,
+        displays the first n rows of the dataset if n is positive, or the last n rows if n is
+        negative."""
+        if not isinstance(n, int) or not isinstance(df, pandas.DataFrame):
+            print("Error: n should be int and df should be pandas.DataFrame")
+            return
+        if abs(n) > df.shape[0]:
+            print("n exceeds the number of rows of the dataset. Displaying all the rows")
+            n = df.shape[0]
+        if n == 0:
+            print("What did you expect ? Nothing to show mate")
+            return
+        if n > 0:
+            print(df.head(n))
+        else:
+            print(df.tail(-n))
--- m04/ex00/test.py
+++ m04/ex00/test.py
@@ -1,4 +1,9 @@
 from FileLoader import FileLoader
 
 fl = FileLoader()
-file = fl.load("../athlete_events.csv")
+df = fl.load("../athlete_events.csv")
+fl.display(df, 32)
+print("")
+fl.display(df, -32)
+print("")
+fl.display(df, 3000000)
